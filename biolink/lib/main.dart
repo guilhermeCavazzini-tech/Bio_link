c7500d4324b747dae5b0a34266b3bb51730e1674
GUILHERME <guilhermescavazzini@outlook.com>
2023-11-17 03:23:52 -0400

    configurando

--- biolink/lib/main.dart
+++ biolink/lib/main.dart
@@ -1,19 +1,18 @@
-
+import 'package:biolink/pages/homepage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(children: [
-        const CircleAvatar(radius: 100,backgroundColor:Colors.black)
-      ]), 
+    return  Material(
+      child: MaterialApp(
+        title: "Biolink",
+        home: MyHome(),
+      ),
     );
   }
 }
